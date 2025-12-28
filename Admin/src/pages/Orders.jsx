f03e32d1a750cfecbb94508ce4117ae0c2097c09
luckytsori8@gmail.com <luckytsori8@gmail.com>
2025-12-28 21:02:08 +0300

    Correcting models for the orders

--- Admin/src/pages/Orders.jsx
+++ Admin/src/pages/Orders.jsx
@@ -25,6 +25,7 @@ const Orders = () => {
     };
     fetchOrders();
   }, []);
+  console.log(orders)
 
   // Update order status
   const handleUpdateOrder = async (id) => {
@@ -69,7 +70,7 @@ const Orders = () => {
   ];
 
   return (
-    <div className="p-5 w-full">
+    <div className="p-5 w-[79vw] mt-8">
       <h1 className="text-2xl font-bold mb-4">All Orders</h1>
       <input
         className="border p-2 mb-4 w-full"
@@ -77,7 +78,7 @@ const Orders = () => {
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
       />
-      <div className="h-[600px]">
+      <div className="h-[500px]">
         <DataGrid
           rows={filteredOrders}
           columns={columns}
